cfa9a65d0a834a320be58ab7a97828a7ffb25789
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-07-29 19:12:59 +0530

    basic syntax in python and indentation

---
+++ 02_python_syntax/main.py
@@ -0,0 +1,10 @@
+print("Hello Shivansh")
+print("I'm getting 10lpa+ Job in August 2025")
+print("Manifesting it right now")
+
+print(15)
+
+# if(a>3):
+#     statement 1
+#     statement 2
+# statement 3
