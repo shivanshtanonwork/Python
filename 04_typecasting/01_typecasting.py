cc0be5f3825b0226756e262966ad63eac551c4fc
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-07-30 11:48:10 +0530

    Typecasting

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,31 @@
+a = 26
+print(a)
+print(type(a))
+
+b = "26"
+print(b)
+print(type(b))
+
+# Convert b to an integer
+c = int(b)
+print(c)
+print(type(c))
+
+d = 1503
+e = str(d)
+print(e)
+print(type(e))
+
+#Examples
+# Convert string to integer
+num_str ="10"
+num_int =int(num_str)
+print(num_int) # Output: 10
+# Convert integer to string
+num =25
+num_str =str(num)
+print(num_str) # Output: "25"
+# Convert float to integer
+pi =3.14
+pi_int =int(pi)
+print(pi_int) # Output: 3
