1f43ec4981c87012ef4510fa700b42b772a3c1dc
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-07 17:29:46 +0530

    lists

---
+++ 12_lists/01_lists.py
@@ -0,0 +1,8 @@
+marks = [54,56,44,59,35]
+# Lists are ordered, changeable(mutable) collection of items
+mixed = [26, "Shivansh", 15, "Tandon"]
+
+print(marks)
+print(mixed[1])
+print(mixed[1:3])
+# print(mixed[4]) # Error Index out of bound
---
+++ 12_lists/02_list_methods.py
@@ -0,0 +1,7 @@
+marks = [20,15,16,18]
+extra_marks = [10,9,14,11]
+
+# marks.append(19) # This will change the original list
+# marks.pop()
+marks.extend(extra_marks)
+print(marks)
