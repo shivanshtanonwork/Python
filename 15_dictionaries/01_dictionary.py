7508f38f772dcb024d73cab76d01f320b2a69701
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-08 12:04:58 +0530

    Dictionaries and dictionary methods

---
+++ 15_dictionaries/01_dictionary.py
@@ -0,0 +1,5 @@
+marks = {"Shivansh": 99, "Vidushi":100, "Aanvee":100}
+# print(marks)
+# print(marks["Shivansh"])
+marks["Vidushi"] = 98
+print(marks)
---
+++ 15_dictionaries/02_dictionary_methods.py
@@ -0,0 +1,7 @@
+marks = {"Shivansh": 99, "Vidushi":100, "Aanvee":100}
+
+print(marks.keys())
+print(marks.values())
+# marks.clear()
+marks.pop("Aanvee")
+print(marks)
